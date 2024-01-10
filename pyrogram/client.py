9ae930034028b788ef9508dceb9840d28122252f
Kuzma Rezchenko <18575606+Kuzma975@users.noreply.github.com>
2024-01-11 00:03:21 +0200

    Extend client with:
     * added handling reaction for replied messages
     * get random message from history
     * reply for the latest message on the application start

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -1,7 +1,9 @@
 #!python
 
 from pyrogram import Client, enums
+from pyrogram import filters
 import configparser
+import random
 
 def init_config(config_file = '../config.ini'):
     config = configparser.ConfigParser()
@@ -31,10 +33,60 @@ async def main():
     async with app:
         # "me" refers to your own chat (Saved Messages)
         async for message in app.get_chat_history("me", limit=1):
-            for entity in message.entities:
-                if entity.type is enums.MessageEntityType.HASHTAG:
-                    print(message.text[entity.offset:entity.offset+entity.length])
+            await app.send_message("me", "What we shold do with this message?", reply_to_message_id=message.id)
+            # await message.reply("What we shold do with this message?")
+        message_count = await app.get_chat_history_count("me")
+        message_ids = list()
+        async for message in app.get_chat_history("me"):
+            message_ids.append(message.id)
+        
+        # async for message in app.get_chat_history("me", offset_id=2, limit=1):
+        #     print(message, end='\n')
+        print(message_count)
+        print(message_ids)
+        random_message_id = random.choice(message_ids)
+        message = await app.get_messages("me", random_message_id)
+        print(message.text)
+        # await app.send_message("me", "this is a reply", reply_to_message_id=random_message_id)
+        # message = next(messages)
+        # for entity in message.entities:
+        #     if entity.type is enums.MessageEntityType.HASHTAG:
+        #         print(message.text[entity.offset:entity.offset+entity.length])
+
 
 config = init_config()
 app = init_client(config)
+emoji = {
+    '👍': '',
+    '✍': 'keep',
+    '👎': '',
+    '💩': '',
+    '🔥': '',
+    '🙈': 'delete',
+    '🤷': 'nothing',
+}
+# app.run(main())
 app.run(main())
+
+@app.on_message()
+async def message_hendler(client, message):
+    print(message)
+
+@app.on_edited_message(filters.chat("me"))
+async def scrap_reaction(client, message):
+    print(f'message are income {message}')
+    if message.reactions:
+        action = emoji[message.reactions.reactions[0].emoji]
+        if action == 'delete':
+            print(message)
+            await client.delete_messages(chat_id=message.chat.id, message_ids=[message.id, message.reply_to_message_id])
+        elif action == 'keep':
+            print(message)
+            await message.reply_to_message.react(emoji="✍")
+            await message.delete()
+        elif action == 'nothing':
+            await message.delete()
+        else:
+            print(f'{action} not implemented for reaction {emoji[message.reactions.reactions[0].emoji]}')
+    # print(message.reactions.reactions[0].emoji)
+app.run()
